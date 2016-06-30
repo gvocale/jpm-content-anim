39de679d9e6d5d7b9aecd46ad4ad679a308bf428
Giovanni <gvocale@gmail.com>
2016-06-30 18:45:07 -0400

    setting down the grid

---
+++ src/js/gradient-map-settings.js
@@ -0,0 +1,6 @@
+// Code used to generate Gradient Map
+
+var target = document.getElementById('myimage');
+var gradientMap = "#242926, #42585c";
+
+GradientMaps.applyGradientMap(target, gradientMap);
